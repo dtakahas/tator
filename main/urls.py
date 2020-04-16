a7300fef0d030fcb545793c02aa095c9a8400884
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-04-16 04:30:57 +0000

    Switch rest documentation URLs over to openapi

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,9 @@ from django.urls import include
 from django.conf.urls import url
 
 from rest_framework.authtoken import views
-from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
-from rest_framework.renderers import DocumentationRenderer
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import MainRedirect
 from .views import ProjectsView
@@ -20,9 +20,16 @@ from .views import AuthAdminView
 
 from .rest import *
 
-class CustomDocs(DocumentationRenderer):
-    template = '/tator_online/main/templates/browser.html'
-    languages = ['javascript']
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tator REST API",
+        default_version='v1',
+        contact=openapi.Contact(email="info@cvisionai.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', MainRedirect.as_view(), name='home'),
@@ -39,9 +46,9 @@ urlpatterns = [
 # This is used for REST calls
 urlpatterns += [
     url(r'^rest/Token', views.obtain_auth_token),
-    url(r'^rest/', include_docs_urls(title='Tator REST API', renderer_classes=[CustomDocs])),
-    # TODO figure out how to not hard code this.
-    url('^schema$', get_schema_view('Tator REST API', url='http://' + os.getenv('MAIN_HOST'))),
+    url(r'^rest(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0)),
+    url(r'^rest/$', schema_view.with_ui('swagger', cache_timeout=0)),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0)),
     path(
         'rest/Algorithms/<int:project>',
         AlgorithmListAPI.as_view(),
