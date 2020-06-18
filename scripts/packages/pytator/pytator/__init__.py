789b4e73e4eaf089dba46ead95602e8814e31a35
Brian Tate <brian.tate@cvisionai.com>
2020-06-17 21:33:07 -0400

    Get notional rtd output working [Hack for old pytator]

--- scripts/packages/pytator/pytator/__init__.py
+++ scripts/packages/pytator/pytator/__init__.py
@@ -3,7 +3,10 @@ Tator Python Bindings REST API
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-from .tator import Tator
-from .tator import Auth
+try:
+    from .tator import Tator
+    from .tator import Auth
+except:
+    pass
 from .version import __version__ as __version__
 
